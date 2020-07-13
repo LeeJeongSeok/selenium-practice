89cfe96d87b245bb44d7b10ba2517da375625ed0
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-14 01:21:59 +0900

    add : daum 자동 로그인 기능 구현

--- src/main/java/com/lee/practice/Selenium.java
+++ src/main/java/com/lee/practice/Selenium.java
@@ -11,7 +11,6 @@ public class Selenium {
     //WebDriver
     private WebDriver driver;
     private WebElement element;
-    private String url;
 
     private String kakaourl = "https://accounts.kakao.com/login?continue=https%3A%2F%2Flogins.daum.net%2Faccounts%2Fksso.do%3Frescue%3Dtrue%26url%3Dhttps%253A%252F%252Fwww.daum.net%252F";
 
@@ -23,6 +22,21 @@ public class Selenium {
     }
 
     public Selenium() {
+
+        selenium_init();
+
+        try {
+            login();
+            Thread.sleep(1000);
+            go_to_mail();
+            Thread.sleep(1000);
+            delete_spam_mail();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void selenium_init() {
         //System Property SetUp
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 
@@ -30,19 +44,31 @@ public class Selenium {
         ChromeOptions options = new ChromeOptions();
         options.setCapability("ignoreProtectedModeSettings", true);
         driver = new ChromeDriver(options);
+    }
 
-        url = "https://nid.naver.com/nidlogin.login?";
-
+    public void login() {
         driver.get(kakaourl);
 
         element = driver.findElement(By.id("id_email_2"));
-        element.sendKeys("ljs0429777@kakao.com");
+        element.sendKeys("test");
 
         //패스워드 입력
         element = driver.findElement(By.id("id_password_3"));
-        element.sendKeys("kjccs123!@#");
+        element.sendKeys("test");
 
         element = driver.findElement(By.className("btn_g"));
         element.click();
     }
+
+    public void go_to_mail() {
+        driver.findElement(By.xpath("//*[@id=\"mArticle\"]/div[1]/div[2]/ul/li[1]/a")).click();
+    }
+
+
+    public void delete_spam_mail() {
+
+        driver.findElement(By.xpath("//*[@id=\"mailToolbar\"]/div/div/div[1]/label")).click();
+        driver.findElement(By.xpath("//*[@id=\"mailToolbar\"]/div/div/div[2]/button[1]")).click();
+    }
 }
+
