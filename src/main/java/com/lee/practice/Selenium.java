759ebeec173f44714e15483f1cce5afeda25645b
LeeJeongSeok <ljs0429777@gmail.com>
2020-07-13 23:12:18 +0900

    add : daum 자동 로그인 기능 구현

--- src/main/java/com/lee/practice/Selenium.java
+++ src/main/java/com/lee/practice/Selenium.java
@@ -13,6 +13,8 @@ public class Selenium {
     private WebElement element;
     private String url;
 
+    private String kakaourl = "https://accounts.kakao.com/login?continue=https%3A%2F%2Flogins.daum.net%2Faccounts%2Fksso.do%3Frescue%3Dtrue%26url%3Dhttps%253A%252F%252Fwww.daum.net%252F";
+
     public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static String WEB_DRIVER_PATH = "/Users/jeongseok/Downloads/chromedriver";
 
@@ -31,16 +33,16 @@ public class Selenium {
 
         url = "https://nid.naver.com/nidlogin.login?";
 
-        driver.get(url);
+        driver.get(kakaourl);
 
-        element = driver.findElement(By.id("id"));
-        element.sendKeys("ljs0429777");
+        element = driver.findElement(By.id("id_email_2"));
+        element.sendKeys("ljs0429777@kakao.com");
 
         //패스워드 입력
-        element = driver.findElement(By.id("pw"));
+        element = driver.findElement(By.id("id_password_3"));
         element.sendKeys("kjccs123!@#");
 
-        element = driver.findElement(By.className("btn_global"));
-        element.submit();
+        element = driver.findElement(By.className("btn_g"));
+        element.click();
     }
 }
